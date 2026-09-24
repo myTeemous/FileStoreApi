a0ea65eff2cd9bb49e7305298e22b7e0fa747d0b
Kenneth Ratliff <k.ratliff3008@gmail.com>
2026-09-24 15:17:56 -0500

    Added User and FileMetaData models

---
+++ Domain/FileMetaData.cs
@@ -0,0 +1,24 @@
+﻿namespace FileStoreApi.Domain
+{
+    public class FileMetadata
+    {
+        public Guid Id { get; set; }
+
+        // Ownership
+        public string UserId { get; set; } = null!;
+        public User User { get; set; } = null!;
+
+        // User-facing information
+        public string FileName { get; set; } = null!;
+        public string ContentType { get; set; } = null!;
+        public long Size { get; set; }
+
+        // Azure Blob information
+        public string BlobContainer { get; set; } = null!;
+        public string BlobName { get; set; } = null!;
+
+        // Metadata
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
