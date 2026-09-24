ab06dc97faac7e34650625a87bb2c587c0d9b6f8
myTeemous <k.ratliff1990@yahoo.com>
2026-09-24 15:50:46 -0500

    adding Folder model

---
+++ Domain/Folder.cs
@@ -0,0 +1,20 @@
+﻿namespace FileStoreApi.Domain
+{
+    public class Folder
+    {
+        public Guid Id { get; set; }
+
+        public string UserId { get; set; } = null!;
+        public User User { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public Guid? ParentFolderId { get; set; }
+        public Folder? ParentFolder { get; set; }
+
+        public ICollection<Folder> Children { get; set; } = [];
+        public ICollection<FileMetadata> Files { get; set; } = [];
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
