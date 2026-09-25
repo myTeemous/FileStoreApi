7b74c5f8d3568170cd42f4fb186d0677c0352f49
myTeemous <k.ratliff1990@yahoo.com>
2026-09-25 09:07:45 -0500

    register Sql Server service

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using FileStoreApi.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<FileStoreApiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FileStoreApiDbConnection"));
+});
 
 var app = builder.Build();
 
